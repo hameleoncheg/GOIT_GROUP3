9d474e4362060eea116e0c07c4f07174a70b274c
annKlush <109134951+annKlush@users.noreply.github.com>
2022-12-04 10:41:00 +0200

    add some new fitch

--- src/main/java/currencyBot/Currency.java
+++ src/main/java/currencyBot/Currency.java
@@ -1,5 +1,48 @@
 package currencyBot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum Currency {
-    USD, UAH, EUR, RUR, BTC
+   // USD, UAH, EUR, RUR;
+
+    USD("USD", true),
+    EUR("EUR", false),
+    UAH("UAH", false);
+    private String currencyName;
+    private boolean currencySelect;
+    Currency(String currency, boolean select) {
+        this.currencyName = currency;
+        this.currencySelect = select;
+    }
+
+    public String getCurrencyName() {
+        return currencyName;
+    }
+    public void setCurrencyName(String currencyName) {
+        this.currencyName = currencyName;
+    }
+    public boolean isCurrencySelect() {
+        return currencySelect;
+    }
+    public void setCurrencySelect(boolean currencySelect) {
+        this.currencySelect = currencySelect;
+    }
+    public static List<Currency> getSelectedCurrencyList() {
+        List<Currency> selectedCurr = new ArrayList<>();
+        for (Currency currency : Currency.values()) {
+            if (currency.currencySelect) {
+                selectedCurr.add(currency);
+            }
+        }
+        return selectedCurr;
+    }
+    public static Currency convertToEnum(String text) {
+        for (Currency currency : Currency.values()) {
+            if (currency.name().equals(text)) {
+                return currency;
+            }
+        }
+        return null;
+    }
 }
