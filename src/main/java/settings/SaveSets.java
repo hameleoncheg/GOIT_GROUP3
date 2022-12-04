eaac63d78bff2ead5764fb0ec147e59d925cfe59
annKlush <109134951+annKlush@users.noreply.github.com>
2022-12-04 10:44:55 +0200

    change

--- src/main/java/settings/SaveSets.java
+++ src/main/java/settings/SaveSets.java
@@ -1,14 +1,8 @@
 package settings;
 
 public class SaveSets implements Runnable{
-    private SetToJson settings;
-
-    public SaveSets(SetToJson settings) {
-        this.settings = settings;
-    }
-
     @Override
     public void run() {
-        settings.save();
+        SetToJson.save();
     }
 }
