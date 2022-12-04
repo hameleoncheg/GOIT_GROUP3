9adc6b27538bb2d3b726607a4adc5495a3622c91
annKlush <109134951+annKlush@users.noreply.github.com>
2022-12-04 10:59:21 +0200

    new function

--- src/main/java/settings/Buttons.java
+++ src/main/java/settings/Buttons.java
@@ -1,31 +1,35 @@
 package settings;
 
 public enum Buttons {
-    START("Старт", "/start"),
-    GET_INFO("Отримати інфо", "Get info"),
-    SETTINGS("Налаштування", "Settings"),
-    NUM_DECIMAL_PLACES("Кількість знаків після коми", "Number of decimal places"),
-    BANK("Банк", "Bank"),
-    CURRENCY("Валюта", "Currency"),
-    NOTIFICATION("Час сповіщення", "Notification"),
-    ZONEID("Часовий пояс", "Time zone"),
-    BACK_TO_SETTINGS("Повернутися", "Settings"),
-    BACK_TO_START("Головне меню", "BACK_TO_START");
+    START ("Старт","/start"),
+    GET_INFO("Отримати інфо","GET_INFO"),
+    SETTINGS("Налаштування","SETTINGS"),
+    NUM_DECIMAL_PLACES ("Кількість знаків після коми","NumDecimalPlaces"),
+    BANK("Банк","Bank"),
+    CURRENCY ("Валюта","Currency"),
+    NOTIFICATION ("Час сповіщення","Notification"),
+    ZONEID("Часовий пояс","ZoneId"),
+    BACK_TO_SETTINGS("↩️","SETTINGS"),
+    BACK_TO_START ("🏠️","BACK_TO_START");
 
-    private String buttonsNameUA;
+    private String buttonsName;
     private String buttonsNameEN;
-    Buttons(String buttonsNameUA, String buttonsNameEN) {
-        this.buttonsNameUA = buttonsNameUA;
+
+    Buttons(String buttonsName, String buttonsNameEN) {
+        this.buttonsName = buttonsName;
         this.buttonsNameEN = buttonsNameEN;
     }
-    public String getNameUA() {
-        return buttonsNameUA;
+
+    public String getName() {
+        return buttonsName;
     }
+
     public String getNameEN() {
         return buttonsNameEN;
     }
-    public static Buttons convertToEnum(String text) {
-        for (Buttons button : Buttons.values()) {
+
+    public static Buttons convertToEnum (String text){
+        for (Buttons button: Buttons.values()) {
             if (button.getNameEN().equals(text)) {
                 return button;
             }
