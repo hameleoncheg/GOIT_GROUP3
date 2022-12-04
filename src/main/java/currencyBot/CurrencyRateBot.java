1fc788c7f4f6fa162521096e266d02e79ec25bed
annKlush <109134951+annKlush@users.noreply.github.com>
2022-12-04 12:23:07 +0200

    menu first point

--- src/main/java/currencyBot/CurrencyRateBot.java
+++ src/main/java/currencyBot/CurrencyRateBot.java
@@ -1,21 +1,45 @@
 package currencyBot;
 
+import menu.MenuStart;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import settings.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class CurrencyRateBot extends TelegramLongPollingBot {
-
-    //
     PrettyResponseConverter converter = new PrettyResponseConverter();
+    private static CurrencyRateBot instance;
+    public String value;
+    private Setting userSettings;
+    private final static Object monitor = new Object();
+    private CurrencyRateBot(String value) {
+        // The following code emulates slow initialization.
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        this.value = value;
+    }
 
+    public static CurrencyRateBot getInstance(String value) {
+        if (instance == null) {
+            instance = new CurrencyRateBot(value);
+        }
+        return instance;
+    }
     @Override
     public String getBotUsername() {
         return Constants.BOT_NAME;
@@ -28,8 +52,22 @@ public class CurrencyRateBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            try {
+                handleMessage(update.getMessage());
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if (update.hasCallbackQuery()) {
+            try {
+                handleQuery(update.getCallbackQuery());
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        /*
         if (update.hasMessage() && update.getMessage().hasText()) {
-
             String messageText = update.getMessage().getText();
             if (messageText.equals("#Monobank") || messageText.equals("#PrivatBank") || messageText.equals("#NBUbank")) {
                 CurrencyRateApiService rateService = getRateService(update.getMessage().getText());
@@ -42,7 +80,7 @@ public class CurrencyRateBot extends TelegramLongPollingBot {
                         )
 
                 );
-                setButtons(message);
+                //setButtons(message);
 
                 try {
                     execute(message); // Call method to send the message
@@ -52,24 +90,63 @@ public class CurrencyRateBot extends TelegramLongPollingBot {
                     e.printStackTrace();
                 }
             }
-        }
+        }*/
     }
 
-    private void setButtons(SendMessage message){
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-        message.setReplyMarkup(replyKeyboardMarkup);
-        replyKeyboardMarkup.setSelective(true);
-        replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboard(false);
-        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+    private void handleQuery(CallbackQuery buttonQuery) throws TelegramApiException {
+        long chatId = buttonQuery.getMessage().getChatId();
+        synchronized (monitor) {
+            if (SetToJson.settings.get(chatId) == null) {
+                userSettings = new Setting(chatId, NumberAfterComa.TWO, Banks.PRIVAT,
+                        Currency.getSelectedCurrencyList(), NotifTime.NINE, TimeZone.UTC_THREE);
+            } else {
+                userSettings = SetToJson.settings.get(chatId);
+            }
+        }
+    }
+    private void handleMessage(Message message) throws TelegramApiException {
+        long chatId = message.getChatId();
+        synchronized (monitor) {
+            if (SetToJson.settings.get(chatId) == null) {
+                userSettings = new Setting(chatId, NumberAfterComa.TWO, Banks.PRIVAT,
+                        Currency.getSelectedCurrencyList(), NotifTime.NINE, TimeZone.UTC_THREE);
+            } else {
+                userSettings = SetToJson.settings.get(chatId);
+            }
+        }
+        if (message.hasText() && message.hasEntities()) {
+            Optional<MessageEntity> commandEntity;
+            commandEntity = message.getEntities().stream()
+                    .filter(e -> "bot_command".equals(e.getType())).findFirst();
+            if (commandEntity.isPresent()) {
+                String command = message.getText()
+                        .substring(commandEntity.get().getOffset(), commandEntity.get().getLength());
+                if (command.equals(Buttons.START.getNameEN())) {
+                    printMessage(chatId, MenuStart.keyboardStart(),
+                            "Ласкаво просимо.Цей бот дозволить відслідкувати актуальні курси валют.");
+                    synchronized (monitor) {
+                        SetToJson.settings.put(chatId, userSettings);
+                    }
+                }
+            }
+        } else {
+            printMessage(chatId, "Будь ласка впишіть /start або натисніть кнопку.");
+        }
+    }
+    private void printMessage(Long chatID, InlineKeyboardMarkup keyboard, String text)
+            throws TelegramApiException {
+        execute(SendMessage.builder()
+                .text(text)
+                .chatId(chatID)
+                .replyMarkup(keyboard)
+                .build());
+    }
 
-        KeyboardRow keyboardFirstRow = new KeyboardRow();
-
-        keyboardFirstRow.add(new KeyboardButton("buy"));
-        keyboardFirstRow.add(new KeyboardButton("sell"));
-        keyboardRowList.add(keyboardFirstRow);
-        replyKeyboardMarkup.setKeyboard(keyboardRowList);
-
+    public void printMessage(Long chatID, String messageText) throws TelegramApiException {
+        execute(SendMessage.builder()
+                .text(messageText)
+                .chatId(chatID)
+                .build());
     }
 
     private CurrencyRateApiService getRateService(String bank){
