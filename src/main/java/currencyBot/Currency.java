b2ab10b6ed841bfc6459899db9e84e250860c1e5
annKlush <109134951+annKlush@users.noreply.github.com>
2022-12-05 20:27:41 +0200

    delete unused field

--- src/main/java/currencyBot/Currency.java
+++ src/main/java/currencyBot/Currency.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum Currency {
-   // USD, UAH, EUR, RUR;
 
     USD("USD", true),
-    EUR("EUR", false),
-    PLN("PLN", false);
+    EUR("EUR", false);
+
     private String currencyName;
     private boolean currencySelect;
     Currency(String currency, boolean select) {
@@ -19,15 +18,7 @@ public enum Currency {
     public String getCurrencyName() {
         return currencyName;
     }
-    public void setCurrencyName(String currencyName) {
-        this.currencyName = currencyName;
-    }
-    public boolean isCurrencySelect() {
-        return currencySelect;
-    }
-    public void setCurrencySelect(boolean currencySelect) {
-        this.currencySelect = currencySelect;
-    }
+   
     public static List<Currency> getSelectedCurrencyList() {
         List<Currency> selectedCurr = new ArrayList<>();
         for (Currency currency : Currency.values()) {
