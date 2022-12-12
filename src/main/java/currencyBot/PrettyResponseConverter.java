893e82a0103671ddf109dba257195cf900419f22
hameleoncheg <hameleoncheg@gmail.com>
2022-12-12 22:31:43 +0200

    final version

--- src/main/java/currencyBot/PrettyResponseConverter.java
+++ src/main/java/currencyBot/PrettyResponseConverter.java
@@ -5,14 +5,14 @@ import java.util.stream.Collectors;
 
 public class PrettyResponseConverter {
 
-    private static final String template = "Курс replCur, купівлі: replRateBay; продажу: replRateSell;";
+    private static final String TEMPLATE = "Курс replCur, купівлі: replRateBay; продажу: replRateSell;";
 
     public static String prepareResponse(List<RateResponseDto> dtos) {
 
         String response = "";
 
         response = dtos.stream()
-                .map(item -> template
+                .map(item -> TEMPLATE
                         .replaceAll("replCur", item.getCurrencyTo().toString())
                         .replaceAll("replRateBay", item.getRateBuy().toString())
                         .replaceAll("replRateSell", item.getRateSell().toString()))
